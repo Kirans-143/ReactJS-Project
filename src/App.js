37527ac2b23f67d547f862267a11f77e4ef8c969
Kirans-143 <kiran.shet77@gmail.com>
2023-01-06 22:08:00 +0530

    Hooks in depth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Counter from "./Counter";
+import Counter from "./Component/Counter";
 
+const getUserData = () => {
+  return fetch("/user.json").then((response) => response.json());
+};
 function App() {
-  const [showCounter, setShowCounter] = useState(false);
+  const [name, setName] = useState();
+  const [roles, setRoles] = useState();
+  const [roleList, setRoleList] = useState();
+  useEffect(() => {
+    if (name) {
+      setRoleList(Object.keys(roles).filter((item) => roles[item]));
+    }
+  }, [name, roles]);
+
+  const loadUser = async () => {
+    const data = await getUserData();
+    console.log("data", data);
+    setName(data.name);
+    setRoles(data.roles);
+  };
+  // const loadUser = () => {
+  //   setName("user1");
+  //   setRoles({
+  //     reader: true,
+  //   });
+  // };
   return (
     <div className="app">
-      {showCounter && <Counter />}
-      <button onClick={() => setShowCounter(true)}>Mount</button>
-      <button onClick={() => setShowCounter(false)}>Unmount</button>
+      {/* <Counter /> */}
+      <h3>{JSON.stringify(name)}</h3>
+      <h3>{JSON.stringify(roles)}</h3>
+      <h3>{JSON.stringify(roleList)}</h3>
+      <button onClick={loadUser}>Load User</button>
     </div>
   );
 }
