9958097bebe83b961f95d4424f950573b4158d60
Kirans-143 <kiran.shet77@gmail.com>
2023-01-07 10:13:15 +0530

    custom hook features

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import Counter from "./Component/Counter";
+import useFetchData from "./customHooks/useFetchData";
 
-const getUserData = () => {
-  return fetch("/user.json").then((response) => response.json());
-};
 function App() {
-  const [name, setName] = useState();
-  const [roles, setRoles] = useState();
-  const [roleList, setRoleList] = useState();
-  useEffect(() => {
-    if (name) {
-      setRoleList(Object.keys(roles).filter((item) => roles[item]));
-    }
-  }, [name, roles]);
+  const [users] = useFetchData("https://jsonplaceholder.typicode.com/users");
+  const [dogData] = useFetchData("https://picsum.photos/id/237/200/300");
 
-  const loadUser = async () => {
-    const data = await getUserData();
-    console.log("data", data);
-    setName(data.name);
-    setRoles(data.roles);
-  };
-  // const loadUser = () => {
-  //   setName("user1");
-  //   setRoles({
-  //     reader: true,
-  //   });
-  // };
+  //for making a request and fetching a resource
+  //it can take more argument but one mandatory argument ----> path of the resource
+  //it returns a promise along with response of that request
+  //we fetch json file accross the network
+  //it returns a representation of that response
+  //so we extract the data json body content from Response object using json()
+
   return (
-    <div className="app">
-      {/* <Counter /> */}
-      <h3>{JSON.stringify(name)}</h3>
-      <h3>{JSON.stringify(roles)}</h3>
-      <h3>{JSON.stringify(roleList)}</h3>
-      <button onClick={loadUser}>Load User</button>
+    <div>
+      {/* <h1>{JSON.stringify(users)}</h1> */}
+      {/* // called as optional chaining */}
+      <h1>{users[0]?.name}</h1>
+      <img src={dogData?.message} />
+      {/* <h1>Hello world</h1> */}
     </div>
   );
 }
